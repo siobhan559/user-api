cfb5ea5ed466ff3af5d5538ded3eb1700783f0b3
Siobhan <siobhanbrown559@gmail.com>
2021-10-10 23:38:40 +0100

    delete controller specs updated

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -5,24 +5,35 @@ RSpec.describe Api::V1::UsersController do
   describe 'Users API', type: :request do
 
     describe 'GET /users' do
-
+      before do
+        FactoryBot.create(:user, username: 'siobhan', email: 'siobhan@test.com', password: 'password', password_confirmation: 'password')
+        FactoryBot.create(:user, username: 'brown', email: 'brown@test.com', password: 'password', password_confirmation: 'password')
+      end
       it 'returns all users' do
-        FactoryBot.create(:user, username: 'siobhan', email: 'siobhan@test.com', password: 'password')
-        FactoryBot.create(:user, username: 'brown', email: 'brown@test.com', password: 'password')
 
         get '/api/v1/users'
 
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body).size).to eq(2)
       end
-
     end
+
+    # describe 'GET /users/:id' do
+    #   it 'returns a user' do
+    #     FactoryBot.create(:user, username: 'siobhan', email: 'siobhan@test.com', password: 'password', password_confirmation: 'password')
+
+    #     get '/api/v1/users/1'
+
+    #     expect(response).to have_http_status(:success)
+    #     # expect(User.find_by_id(1)).to eq(1)
+    #   end
+    # end
 
     describe 'POST /users' do
 
       it 'creates a new user' do
         expect {
-          post '/api/v1/users', params: {user: {username: 'siobhan', email: 'siobhan@test.com', password: 'password'}}
+          post '/api/v1/users', params: {user: {username: 'siobhan', email: 'siobhan@test.com', password: 'password', password_confirmation: 'password'}}
         }.to change { User.count }.from(0).to(1)
 
         expect(response).to have_http_status(:created)
@@ -30,9 +41,13 @@ RSpec.describe Api::V1::UsersController do
     end
 
     describe 'DELETE /users/:id' do
+      let!(:user) { FactoryBot.create(:user, username: 'siobhan', email: 'siobhan@test.com', password: 'password', password_confirmation: 'password') }
+
       it 'deletes a user' do
-        FactoryBot.create(:user, username: 'siobhan', email: 'siobhan@test.com', password: 'password')
-        delete '/api/v1/users/1'
+        expect {
+          delete "/api/v1/users/#{user.id}",
+          headers: {"Authorization" => ENV["API_KEY"]}
+        }.to change { User.count }.from(1).to(0)
 
         expect(response).to have_http_status(:no_content)
       end
